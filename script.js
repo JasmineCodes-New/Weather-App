f470e45dff8cbe3916071c18e7914b1f796c276f
Jasmine Irvin <104696443+JasmineCodes-New@users.noreply.github.com>
2022-05-22 18:06:53 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name; 
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"; 
         document.querySelector(".description").innerText = description; 
-        document.querySelector(".temp").innerText = temp + "°F"; 
+        document.querySelector(".temp").innerText = temp + "°C"; 
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%"; 
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "mph"; 
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h"; 
         document.querySelector(".weather").classList.remove("loading"); 
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"; 
     },
